f42cdd4f85c521522107ebd5764060ad5eb04b5e
TheFatFatCoder <thefatflyer@gmail.com>
2020-09-19 13:40:28 +0700

    Separated diagnosis module

--- diagnosis_module/app.js
+++ diagnosis_module/app.js
@@ -35,77 +35,15 @@ app.use((err, req, res, next)=>{
     }
 });
 
-const PORT = process.env.USER_PORT
+const PORT = process.env.DIAGNOSIS_PORT
 const host = process.env.MY_SQL_HOST
 const user = process.env.MY_SQL_USER
 const password = typeof process.env.MY_SQL_PASSWORD === 'undefined' ? '' : process.env.MY_SQL_PASSWORD
 const dbname = process.env.MY_SQL_DBNAME
 const dao = new Dao(host, user, password, dbname)
 
-app.get("/api/user/retrieve-users", (req, res)=>{
-    if (typeof req.query.id === 'undefined'){
-        // RETRIEVE ALL
-        dao.retrieveUsers().then(result=>{
-            res.status(200).send({
-                success: true,
-                result: result
-            })
-        }).catch(err=>{
-            console.log(err)
-            res.status(500).send({
-                success: false,
-                result: SOMETHING_WENT_WRONG
-            })
-        })
-    }else{
-        // RETRIEVE WITH ID
-    }
-})
 
-app.post("/api/user/register-user", (req, res)=>{
-    if (typeof req.body.fullname === 'undefined' ||
-        typeof req.body.mobile === 'undefined' ||
-        typeof req.body.email === 'undefiend' ||
-        typeof req.body.birthdate === 'undefined' ||
-        typeof req.body.password === 'undefined'){
-        res.status(400).send({
-            success: false,
-            error: WRONG_BODY_FORMAT
-        })
-        return
-    }else{
-        const user = new User(null,
-            req.body.fullname,
-            req.body.mobile,
-            req.body.email,
-            req.body.birthdate,
-            req.body.password,
-            "CUSTOMER")
-
-        dao.registerCustomer(user).then(result=>{
-            res.status(200).send({
-                success: true,
-                result: result
-            })
-        }).catch(err=>{
-            if (err.code === 'ER_DUP_ENTRY') {
-                res.status(200).send({
-                    success: false,
-                    message: 'DUPLICATE-ENTRY'
-                })
-                res.end()
-            }else{
-                console.log(err)
-                res.status(500).send({
-                    success: false,
-                    result: SOMETHING_WENT_WRONG
-                })
-            }
-        })
-    }
-})
-
-app.get("/api/user/retrieve-animal-category", (req, res)=>{
+app.get("/api/diagnosis/retrieve-animal-category", (req, res)=>{
     if (typeof req.query.id === 'undefined'){
         // RETRIEVE ALL
         dao.retrieveAnimalCategory().then(result=>{
@@ -125,7 +63,7 @@ app.get("/api/user/retrieve-animal-category", (req, res)=>{
     }
 })
 
-app.post("/api/user/add-animal-category", (req, res)=>{
+app.post("/api/diagnosis/add-animal-category", (req, res)=>{
     if (typeof req.body.category_name === 'undefined'){
         res.status(400).send({
             success: false,
@@ -158,7 +96,7 @@ app.post("/api/user/add-animal-category", (req, res)=>{
     }
 })
 
-app.get("/api/user/retrieve-animal-type", (req, res)=>{
+app.get("/api/diagnosis/retrieve-animal-type", (req, res)=>{
     dao.retrieveAnimalType().then(result=>{
         res.status(200).send({
             success: true,
@@ -173,7 +111,7 @@ app.get("/api/user/retrieve-animal-type", (req, res)=>{
     })
 })
 
-app.post("/api/user/add-animal-type", (req, res)=>{
+app.post("/api/diagnosis/add-animal-type", (req, res)=>{
     if (typeof req.body.category_id === 'undefined' ||
         typeof req.body.animal_name === 'undefined'){
         res.status(400).send({
