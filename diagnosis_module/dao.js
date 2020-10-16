59a326cad1280f2b2d0d45b7fa435f45764d1334
TheFatFatCoder <thefatflyer@gmail.com>
2020-10-16 08:32:13 +0700

    Reverted retrieveOneAnimal function block

--- diagnosis_module/dao.js
+++ diagnosis_module/dao.js
@@ -189,27 +189,33 @@ export class Dao{
 
 	retrieveOneAnimalCategory(animalCategory){
 		return new Promise((resolve,reject)=>{
-			const query="SELECT at.id, at.animal_name, at.animal_category_id, ac.category_name FROM animal_type at INNER JOIN animal_category ac ON at.animal_category_id = ac.id WHERE at.animal_category_id=?"
-			//const query="SELECT * FROM animal_category WHERE id=?"
+			const query = "SELECT category_name FROM animal_category WHERE id = ?"
 			this.mysqlConn.query(query, [animalCategory.id], (err, res)=>{
-				if (err){
-					reject(err)
+				if (res.length === 0){
+					resolve([])
 					return
 				}
+				const category_name = res[0].category_name
+				const query="SELECT at.id, at.animal_name, at.animal_category_id, ac.category_name FROM animal_type at INNER JOIN animal_category ac ON at.animal_category_id = ac.id WHERE at.animal_category_id=?"
+				this.mysqlConn.query(query, [animalCategory.id], (err, res)=>{
+					if (err){
+						reject(err)
+						return
+					}
 
-				let animals = []
-				let category_name = res[0].category_name
-				for	(let i=0; i<res.length; i++){
-					animals.push(new AnimalType(
-						res[i].id,
-						res[i].animal_name,
-						new AnimalCategory(res[i].animal_category_id, res[i].category_name)
-					))
-				}
+					let animals = []
+					for	(let i=0; i<res.length; i++){
+						animals.push(new AnimalType(
+							res[i].id,
+							res[i].animal_name,
+							new AnimalCategory(res[i].animal_category_id, res[i].category_name)
+						))
+					}
 
-				resolve({
-					category_name: category_name,
-					animal_types: animals
+					resolve({
+						category_name: category_name,
+						animal_types: animals
+					})
 				})
 			})
 		})
