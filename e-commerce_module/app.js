7308f3678a26a3818d9b1625d879eae824162a3e
Ferdinand <ferdinandgregorius@yahoo.com>
2020-12-10 19:50:49 +0700

    Added Decline Transaction endpoint

--- e-commerce_module/app.js
+++ e-commerce_module/app.js
@@ -501,6 +501,44 @@ app.post("/api/ecommerce/approve-transaction",(req,res)=>{
     })
 })
 
+app.post("/api/ecommerce/decline-transaction",(req,res)=>{
+    if(typeof req.body.id==='undefined'){
+        res.status(400).send({
+            success:false,
+            error:WRONG_BODY_FORMAT
+        })
+        return
+    }
+
+    dao.retrieveOneTransaction(req.body.id).then(result=>{
+        dao.declineTransaction(new Transaction(req.body.id)).then(result=>{
+            res.status(200).send({
+                success:true,
+                result:result
+            })
+        }).catch(error=>{
+            console.error(error)
+            res.status(500).send({
+                success:false,
+                error:SOMETHING_WENT_WRONG
+            })
+        })
+    }).catch(error=>{
+        if(error===NO_SUCH_CONTENT){
+            res.status(204).send({
+                success:false,
+                error:NO_SUCH_CONTENT
+            })
+            return
+        }
+        console.error(error)
+        res.status(500).send({
+            success:false,
+            error:SOMETHING_WENT_WRONG
+        })
+    })
+})
+
 app.listen(PORT, ()=>{
     console.info(`Server serving port ${PORT}`)
 })
