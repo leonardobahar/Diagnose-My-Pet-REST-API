7b11db24a897016767fdd82a828cf9d58faa665b
Ferdinand <ferdinandgregorius@yahoo.com>
2021-02-17 21:11:47 +0700

    Added retrieveAppointmentByDoctorName function block

--- user_module/dao.js
+++ user_module/dao.js
@@ -1122,6 +1122,49 @@ export class Dao{
 		})
 	}
 
+	retrieveAppointmentByDoctorName(doctor){
+		return new Promise((resolve,reject)=>{
+			if(!doctor instanceof Doctor){
+				reject(MISMATCH_OBJ_TYPE)
+				return
+			}
+
+			const query="SELECT a.id, a.appointment_name, a.appointment_time, a.duration, a.appointment_status, a.user_id, " +
+				"a.is_real_appointment, u.user_name, a.patient_id, p.patient_name, a.doctor_id, d.doctor_name " +
+				"FROM appointment a LEFT OUTER JOIN users u ON a.user_id=u.id " +
+				"LEFT OUTER JOIN patients p ON a.patient_id=p.id " +
+				"LEFT OUTER JOIN doctor d ON a.doctor_id=d.id " +
+				"WHERE d.doctor_name=? "
+			this.mysqlConn.query(query,doctor.doctor_name,(error,result)=>{
+				if(error){
+					reject(error)
+					return
+				}else if(result.length>0){
+					const schedule=result.map(rowDataPacket=>{
+						const appointmentTime =  moment(rowDataPacket.appointment_time, 'YYYY/MM/DD HH:mm:ss').format("YYYY-MM-DD HH:mm:ss");
+						return{
+							id:rowDataPacket.id,
+							appointment_name:rowDataPacket.appointment_name,
+							appointment_time:appointmentTime,
+							duration:rowDataPacket.duration,
+							appointment_status:rowDataPacket.appointment_status,
+							user_id:rowDataPacket.user_id,
+							user_name:rowDataPacket.user_name,
+							is_real_appointment:rowDataPacket.is_real_appointment,
+							patient_id:rowDataPacket.patient_id,
+							pet_name:rowDataPacket.patient_name,
+							doctor_id:rowDataPacket.doctor_id,
+							doctor_name:rowDataPacket.doctor_name
+						}
+					})
+					resolve(schedule)
+				}else{
+					reject(NO_SUCH_CONTENT)
+				}
+			})
+		})
+	}
+
 	retrieveAppointmentsBetweenDates(date1,date2){
 		return new Promise((resolve,reject)=>{
 			const query="SELECT a.id, a.appointment_name, a.appointment_time, a.duration, a.appointment_status, a.user_id, " +
