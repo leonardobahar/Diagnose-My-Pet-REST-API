80db2836f099545c160287409344e9c2160a2225
Ferdinand <ferdinandgregorius@yahoo.com>
2021-02-20 15:07:24 +0700

    Re-modified retrieve appointment endpoint to retrieve with customer id

--- user_module/app.js
+++ user_module/app.js
@@ -1678,7 +1678,25 @@ app.get("/api/user/retrieve-appointment", (req,res)=>{
             })
         })
     }else if(typeof req.query.customer_id !=='undefined'){
-
+        dao.retrieveAppointmentByCustomerId(new User(req.query.customer_id)).then(result=>{
+            res.status(200).send({
+                success:true,
+                result:result
+            })
+        }).catch(error=>{
+            if(error===NO_SUCH_CONTENT){
+                res.status(204).send({
+                    success:false,
+                    error:NO_SUCH_CONTENT
+                })
+                return
+            }
+            console.error(error)
+            res.status(500).send({
+                success:false,
+                error:SOMETHING_WENT_WRONG
+            })
+        })
     } else{
         const appointment=new Appointment(req.query.id,null,null,null,null)
         dao.retrieveOneAppointment(appointment).then(result=>{
