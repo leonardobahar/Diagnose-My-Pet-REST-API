1e20c20ed05baf6eafc379cadabb16dbb9149692
leonardobahar <thefatflyer@gmail.com>
2021-03-15 20:45:35 +0700

    Console.log fuckedupHandler

--- util/util.js
+++ util/util.js
@@ -6,7 +6,7 @@ export const generateAccessToken = (userInfo, token_secret)=>{
 
 export class AgeFormatter{
     constructor(date1, date2){
-        if (!date1 instanceof Date || !date2 instanceof Date){
+        if (!date1.getDate() || !date2.getDate()){
             this.fckedUp = true
         }else{
             this.fckedUp = false
