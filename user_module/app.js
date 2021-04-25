b2c88c2c327d7596c37164dd0c810417c7da2428
leonardobahar <thefatflyer@gmail.com>
2021-04-25 14:12:56 +0700

    Fixed double booking on use-appointment-slot

--- user_module/app.js
+++ user_module/app.js
@@ -2639,7 +2639,7 @@ app.post("/api/user/use-appointment-slot",(req, res)=>{
 
         dao.retrieveOneAppointmentSchedule(req.body.appointment_id).then(appointmentResult=>{
             console.log(appointmentResult)
-            if (appointmentResult.patient_id !== null ){
+            if (appointmentResult[0].patient_id !== null ){
                 res.status(500).send({
                     success: false,
                     error: "SLOT HAS BEEN TAKEN"
