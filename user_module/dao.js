a242e7c910d833cbe7cd94582e834d8fd4ea382d
Ferdinand <ferdinandgregorius@yahoo.com>
2021-05-31 19:39:24 +0700

    Added updateAppointmentSlot function block in user_module dao.js

--- user_module/dao.js
+++ user_module/dao.js
@@ -3091,6 +3091,19 @@ export class Dao{
 		})
 	}
 
+	updateAppointmentSlot(appointment_id, patient_id, proof_of_payment, description, additional_storage){
+		return new Promise((resolve,reject)=>{
+			const query="UPDATE v2_appointment_schedule SET description=?, additional_storage=?, status='UPDATED', patient_id=?, proof_of_payment=? WHERE id=? "
+			this.mysqlConn.query(query,[description,additional_storage,patient_id,proof_of_payment,appointment_id],(err,res)=>{
+				if(!err){
+					resolve(SUCCESS)
+				}else{
+					reject(err)
+				}
+			})
+		})
+	}
+
 	approveAppointmentSlot(appointment_id){
 		return new Promise((resolve,reject)=>{
 			const query="UPDATE v2_appointment_schedule SET status='APPROVED' WHERE id=? "
