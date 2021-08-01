23b45d7e586dd3fa074f37822ffc9478f2327c3b
leonardobahar <thefatflyer@gmail.com>
2021-08-01 19:14:55 +0700

    Added codoctor info

--- user_module/dao.js
+++ user_module/dao.js
@@ -2792,9 +2792,9 @@ export class Dao{
 
 	retrieveOneAppointmentSchedule(id){
 		return new Promise((resolve,reject)=>{
-			const query="SELECT a.id, a.start_time, a.end_time, a.proof_of_payment, a.description, a.additional_storage, a.status, a.doctor_id, d.doctor_name, a.patient_id, p.patient_name, p.birthdate, p.breed, p.pet_owner_id, u.user_name, u.mobile, a.booking_type_name, bt.duration, " +
+			const query="SELECT a.id, a.start_time, a.end_time, a.proof_of_payment, a.description, a.additional_storage, a.status, a.doctor_id, d.doctor_name, a.co_doctor_id, cd.doctor_name as co_doctor_name, a.patient_id, p.patient_name, p.birthdate, p.breed, p.pet_owner_id, u.user_name, u.mobile, a.booking_type_name, bt.duration, " +
 				"mr.description, mr.medication, mr.date_created " +
-				"FROM v2_appointment_schedule a LEFT OUTER JOIN doctor d ON a.doctor_id=d.id LEFT OUTER JOIN patients p ON a.patient_id=p.id LEFT OUTER JOIN v2_booking_type bt ON bt.booking_type_name=a.booking_type_name LEFT OUTER JOIN users u ON u.id=p.pet_owner_id " +
+				"FROM v2_appointment_schedule a LEFT OUTER JOIN doctor d ON a.doctor_id=d.id LEFT OUTER JOIN doctor cd ON a.co_doctor_id = cd.id LEFT OUTER JOIN patients p ON a.patient_id=p.id LEFT OUTER JOIN v2_booking_type bt ON bt.booking_type_name=a.booking_type_name LEFT OUTER JOIN users u ON u.id=p.pet_owner_id " +
 				"LEFT OUTER JOIN medical_records mr ON mr.appointment_id=a.id " +
 				"WHERE a.id = ? "+
 				"ORDER BY a.start_time ASC "
@@ -2821,6 +2821,8 @@ export class Dao{
 							status:rowDataPacket.status,
 							doctor_id:rowDataPacket.doctor_id,
 							doctor_name:rowDataPacket.doctor_name,
+							co_doctor_id:rowDataPacket.co_doctor_id,
+							co_doctor_name:rowDataPacket.co_doctor_name,
 							patient_id:rowDataPacket.patient_id,
 							patient_name:rowDataPacket.patient_name,
 							birthdate:d1,
