9fc5e9ebced50caf4a8872fcced1795937de2cdd
Ferdinand <ferdinandgregorius@yahoo.com>
2021-08-02 12:07:26 +0700

    Fixed /api/user/use-appointment-slot endpoint by re-applying new Upload format

--- user_module/app.js
+++ user_module/app.js
@@ -2345,18 +2345,18 @@ app.get("/api/user/retrieve-doctor-by-booking-type",(req,res)=>{
                 result:result
             })
         }).catch(error=>{
-            if (err===ERROR_DUPLICATE_ENTRY){
+            if (error===ERROR_DUPLICATE_ENTRY){
                 res.status(500).send({
                     success: false,
                     error: ERROR_DUPLICATE_ENTRY
                 })
-            }else if(err===NO_SUCH_CONTENT){
+            }else if(error===NO_SUCH_CONTENT){
                 res.status(204).send({
                     success:false,
                     error:NO_SUCH_CONTENT
                 })
             } else {
-                console.error(err)
+                console.error(error)
                 res.status(500).send({
                     success: false,
                     error: SOMETHING_WENT_WRONG
@@ -2654,31 +2654,50 @@ app.post("/api/user/add-appointment-slot", (req, res)=>{
     })
 })
 
-app.post("/api/user/use-appointment-slot",(req, res)=>{
-    const upload=multer({storage:storage, fileFilter: medicalRecordFilter}).single('payment_attachment')
+app.post("/api/user/use-appointment-slot",upload.single('payment_attachment'),(req, res)=>{
 
-    upload(req,res,async(error)=>{
-        if (typeof req.body.appointment_id === 'undefined' ||
-            typeof req.body.patient_id === 'undefined'
-        ){
-            res.status(400).send({
-                success:false,
-                error:WRONG_BODY_FORMAT
+    if (typeof req.body.appointment_id === 'undefined' ||
+        typeof req.body.patient_id === 'undefined'
+    ){
+        res.status(400).send({
+            success:false,
+            error:WRONG_BODY_FORMAT
+        })
+        return
+    }
+
+    dao.retrieveOneAppointmentSchedule(req.body.appointment_id).then(appointmentResult=>{
+        if (appointmentResult[0].patient_id !== null ){
+            res.status(500).send({
+                success: false,
+                error: "SLOT HAS BEEN TAKEN"
             })
             return
         }
+        dao.retrieveBookingTypeByName(appointmentResult[0].booking_type_name).then(bookingResult=>{
+            if(bookingResult[0].payment_proof_required ===1){
+                if(typeof req.file==='undefined'){
+                    res.status(400).send({
+                        success:false,
+                        error:'Proof of payment is required for this booking type'
+                    })
+                    return
+                }
 
-        dao.retrieveOneAppointmentSchedule(req.body.appointment_id).then(appointmentResult=>{
-            if (appointmentResult[0].patient_id !== null ){
-                res.status(500).send({
-                    success: false,
-                    error: "SLOT HAS BEEN TAKEN"
-                })
-                return
-            }
+                const imageInputAbsPath=`${'./Uploads/'}Uncompressed/${req.file.filename}`
+                compressImages(imageInputAbsPath,`${'./Uploads/'}`,{compress_force:false,statistic:false,autoupdate:true},
+                    false,{jpg:{engine:"mozjpeg",command:["-quality","60"]}},
+                    {png:{engine:"pngquant",command:["--quality=20-50","-o"]}},
+                    {svg:{engine:"svgo",command:"--multipass"}},
+                    {gif:{engine:"gifsicle",command:["--colors","64","--use-col=web"]}},
+                    function(error,completed){
+                        if(completed===true){
+                            fs.unlinkSync(imageInputAbsPath)
+                        }
+                    }
+                )
 
-            if(typeof req.file==='undefined' || req.file === null){
-                dao.useAppointmentSlot(req.body.appointment_id, req.body.patient_id, null, req.body.description, req.body.additional_question).then(result=>{
+                dao.useAppointmentSlot(req.body.appointment_id, req.body.patient_id, req.file.filename, req.body.description, req.body.additional_question).then(result=>{
                     if (result.affectedRows === 0){
                         res.status(404).send({
                             success: false,
@@ -2711,34 +2730,53 @@ app.post("/api/user/use-appointment-slot",(req, res)=>{
                         })
                     }
                 })
-            }else {
-                // File exist
-                dao.useAppointmentSlot(req.body.appointment_id, req.body.patient_id, req.file.filename, req.body.description, req.body.additional_question).then(result => {
-                    if (result.affectedRows === 0) {
+            }else{
+
+                let filename;
+                if  (typeof req.file === 'undefined'){
+                    filename = null
+                }else{
+                    const imageInputAbsPath=`${'./Uploads/'}Uncompressed/${req.file.filename}`
+                    compressImages(imageInputAbsPath,`${'./Uploads/'}`,{compress_force:false,statistic:false,autoupdate:true},
+                        false,{jpg:{engine:"mozjpeg",command:["-quality","60"]}},
+                        {png:{engine:"pngquant",command:["--quality=20-50","-o"]}},
+                        {svg:{engine:"svgo",command:"--multipass"}},
+                        {gif:{engine:"gifsicle",command:["--colors","64","--use-col=web"]}},
+                        function(error,completed){
+                            if(completed===true){
+                                fs.unlinkSync(imageInputAbsPath)
+                            }
+                        }
+                    )
+                    filename = req.file.filename
+                }
+
+                dao.useAppointmentSlot(req.body.appointment_id, req.body.patient_id, filename, req.body.description, req.body.additional_question).then(result=>{
+                    if (result.affectedRows === 0){
                         res.status(404).send({
                             success: false,
                             message: ERROR_FOREIGN_KEY
                         })
-                    } else {
-                        dao.addAppointmentLog(req.body.patient_id, appointmentResult[0].booking_type_name, appointmentResult[0].start_time, req.body.notes).then(result => {
+                    }else{
+                        dao.addAppointmentLog(req.body.patient_id,appointmentResult[0].booking_type_name,appointmentResult[0].start_time,req.body.notes).then(result=>{
                             res.status(200).send({
                                 success: true
                             })
-                        }).catch(error => {
+                        }).catch(error=>{
                             console.error(error)
                             res.status(500).send({
-                                success: false,
-                                error: SOMETHING_WENT_WRONG
+                                success:false,
+                                error:SOMETHING_WENT_WRONG
                             })
                         })
                     }
-                }).catch(err => {
-                    if (err.code === "ER_NO_REFERENCED_ROW_2") {
+                }).catch(err=>{
+                    if (err.code==="ER_NO_REFERENCED_ROW_2"){
                         res.status(404).send({
                             success: false,
                             message: ERROR_FOREIGN_KEY
                         })
-                    } else {
+                    }else{
                         console.error(err)
                         res.status(500).send({
                             success: false,
@@ -2753,13 +2791,25 @@ app.post("/api/user/use-appointment-slot",(req, res)=>{
                     success:false,
                     error:NO_SUCH_CONTENT
                 })
-                return
             }
             console.error(error)
             res.status(500).send({
                 success:false,
                 error:SOMETHING_WENT_WRONG
             })
+        })
+    }).catch(error=>{
+        if(error===NO_SUCH_CONTENT){
+            res.status(204).send({
+                success:false,
+                error:NO_SUCH_CONTENT
+            })
+            return
+        }
+        console.error(error)
+        res.status(500).send({
+            success:false,
+            error:SOMETHING_WENT_WRONG
         })
     })
 })
